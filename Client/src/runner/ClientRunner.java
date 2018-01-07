a29d435383d8bab8b6d51cea3a59122bf16524f0
161250203 <161250203@mail.smail.nju.edu.cn>
2018-01-08 00:45:05 +0800

    Test RMI

--- Client/src/runner/ClientRunner.java
+++ Client/src/runner/ClientRunner.java
@@ -1,12 +1,20 @@
 package runner;
 
+import java.net.MalformedURLException;
+import java.rmi.Naming;
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import rmi.RemoteHelper;
 
 public class ClientRunner extends Application {
+	
+	private static RemoteHelper remoteHelper;
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
@@ -17,8 +25,23 @@ public class ClientRunner extends Application {
 			primaryStage.show();
 			presentation.userUI.Loginui.LoginStage=primaryStage;
 	}
+	
+	private static void linkToServer() {
+		try {
+			remoteHelper = RemoteHelper.getInstance();
+			remoteHelper.setRemote(Naming.lookup("rmi://127.0.0.1:8887/DataRemoteObject"));
+			System.out.println("linked");
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		} catch (RemoteException e) {
+			e.printStackTrace();
+		} catch (NotBoundException e) {
+			e.printStackTrace();
+		}
+	}
 
 	public static void main(String[] args) {
+		linkToServer();
 		launch(args);
 	}
 
